4e635d2ad7d6de657f740da56f400cd424e87317
rustzz-27 <reshuag19@gmail.com>
2024-07-01 18:30:20 +0100

    Frontend Change: 1. Added axios library 2. Added basic html

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  query: string = '';
+  suggestions: any[] = [];
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // this.fetchData ();
+  }
+
+  fetchData() {
+    this.http.get<any[]>('http://localhost:5000/data')
+      .subscribe(
+        response => this.data = response,
+        error => console.error('Error fetching data', error)
+      );
+  }
+
+  onSearchChange(value: string) {
+    this.query = value;
+    if (value.length > 2) {
+      this.suggestions = this.data.filter(item => 
+        item.name.toLowerCase().includes(value.toLowerCase()) ||
+        item.albums.some((album:any) => album.title.toLowerCase().includes(value.toLowerCase())) ||
+        item.albums.some((album:any) => album.songs.some((song:any) => song.title.toLowerCase().includes(value.toLowerCase())))
+      );
+    } else {
+      this.suggestions = [];
+    }
+  }
 }
---
+++ app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
